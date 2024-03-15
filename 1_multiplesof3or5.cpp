b62d5046ddaddd0938addfcd2b664995e8aee9bc
Adheesh <adheeshchat@gmail.com>
2024-03-15 13:28:25 -0700

    solved p2, modified description in p1

--- 1_multiplesof3or5.cpp
+++ 1_multiplesof3or5.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <unordered_set>
 #include <cassert>
+
 /*
-PROBLEM STATEMENT
+Problem 1: Multiples of 3 or 5
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
 Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
 */
+
 int multiplesOf3Or5(int number){
     std::unordered_set<int> s1;
 
